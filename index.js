daebef2dccdcdd4b3e6ded8009d8531178ba370b
Vinicius Figueiredo Rodrigues <vinicius.figueiredo@linx.com.br>
2018-07-27 22:39:02 -0300

    Add redis package and business

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 const express = require('express');
+const redisFactory = require('redis');
+
 const app = express();
+const redisClient = redisFactory.createClient();
+
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Hello, World!'
-    });
+const ROUTES = {
+    BASE: '/',
+    EXAMPLE: '/example'
+}
+
+const redis = {
+    get (key, defaultValue) {
+        return new Promise ((resolve, reject) => {
+            redisClient.get(key, (err, reply) => {
+                if (err) {
+                    return reject(err);
+                }
+                if (reply) {
+                    console.log(`Redis: GET ${key} = ${reply}`)
+                    return resolve(reply)
+                }
+                resolve(defaultValue);
+            })
+        })
+    },
+    set (key, value) {
+        redisClient.set(key, value);
+    }
+}
+
+
+app.get(ROUTES.BASE, (req, res, next) => {
+    redis.get(ROUTES.BASE, '0')
+        .then((countString)=>{
+            let countAccess = parseInt(countString) + 1;
+            redis.set(ROUTES.BASE, countAccess.toString());
+            res.status(200).json({
+                countAccess,
+                message: 'Hello, World!'
+            });
+        })
+});
+
+app.get(ROUTES.EXAMPLE, (req, res, next) => {
+    redis.get(ROUTES.EXAMPLE, '0')
+        .then((countString)=>{
+            let countAccess = parseInt(countString) + 1;
+            redis.set(ROUTES.EXAMPLE, countAccess.toString());
+            res.status(200).json({
+                countAccess,
+                message: 'EXAMPLE ROUTE!'
+            });
+        })
 });
 
 app.listen(PORT, () => {
